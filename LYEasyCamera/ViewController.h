87fa77c75396ce6db840371a46f37f852d6edb9a
LYoung05 <yuanliyong520@126.com>
2018-04-10 17:52:47 +0800

    Initial Commit

---
+++ LYEasyCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYEasyCamera
+//
+//  Created by CNFOL_iOS on 2018/4/10.
+//  Copyright © 2018年 LYOUNG_iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
